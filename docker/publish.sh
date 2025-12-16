f26331a165c46dc68ae954d8e70783fdd73c57d4
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-12-16 20:09:57 +0400

    Update serve-grounding-dino: supervisely 6.73.486, instance 6.15.35 (#2)

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/grounding_dino:1.0.0 . && \
-docker push supervisely/grounding_dino:1.0.0
+docker build -t supervisely/grounding_dino:1.0.1 . && \
+docker push supervisely/grounding_dino:1.0.1
